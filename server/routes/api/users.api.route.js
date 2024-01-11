922773ace896bf19743e3cd7779683a3bd8c34c2
pompidou41 <musaev5115@gmail.com>
2024-01-11 18:05:41 +0300

    auth_route

--- server/routes/api/users.api.route.js
+++ server/routes/api/users.api.route.js
@@ -1,9 +1,15 @@
 const router = require('express').Router();
 const { User } = require('../../db/models');
 
-router.get('/', async (req, res) => {
-  const users = await User.findAll();
-  res.status(200).json({ message: 'success', users });
+router.get('/:tournament_id', async (req, res) => {
+  const { tournament_id } = req.params;
+  try {
+    const users = await User.findAll({ where: { tournament_id } });
+    return res.status(200).json({ message: 'success', users });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: error.message });
+  }
 });
 
 module.exports = router;
